a5b83c3565b582f138e9dab75f3c74796d673b20
Wyatt James <wyattsamueljames@gmail.com>
2025-07-15 12:09:04 -0600

    feat(database): add connect to db

--- pkg/database/queries.go
+++ pkg/database/queries.go
@@ -2,10 +2,19 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 )
 
 type Database struct {
 	URL string
+}
+
+func (d Database) Connect() (*sql.DB, error) {
+	db, err := sql.Open("libsql", d.URL)
+	if err != nil {
+		return nil, errors.New("Could not open connection to database.")
+	}
+	return db, nil
 }
