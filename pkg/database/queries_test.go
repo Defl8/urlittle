c4031a74e2d708035b71c8afa3020be14f77f2eb
Wyatt James <wyattsamueljames@gmail.com>
2025-07-16 12:32:29 -0600

    chore(database): update tests

--- pkg/database/queries_test.go
+++ pkg/database/queries_test.go
@@ -41,8 +41,30 @@ func TestExecuteQuery(t *testing.T) {
 		t.Errorf("Could not load .env file. ERROR: %v", err)
 	}
 	rows, err := dbInst.ExecQuery("SELECT id FROM urls")
-	defer rows.Close()
 	if err != nil {
 		t.Errorf("Could not query the database. ERROR: %v", err)
+	}
+	defer rows.Close()
+}
+
+func TestGetURLs(t *testing.T) {
+	expected_urls := []*URL{newURL(1, "test", "test", "2025-07-16")}
+
+	dbInst, err := Setup()
+	if err != nil {
+		t.Errorf("Could not load .env file. ERROR: %v", err)
+	}
+	urls, err := dbInst.GetURLs()
+	if err != nil {
+		t.Errorf("Error getting URLs. ERROR: %v", err)
+	}
+	for i := range urls {
+		fmt.Printf("Index %d expected URL: %+v\n", i, expected_urls[i])
+		fmt.Printf("Index %d actual URL: %+v\n", i, urls[i])
+
+		// Need to be dereferenced in order to compare by struct members.
+		if *urls[i] != *expected_urls[i] {
+			t.Error("URLs do not match expected")
+		}
 	}
 }
